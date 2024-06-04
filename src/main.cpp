1fe06d1b9a4a0a4d455e7e121fd044bb6535e056
Kenichi Wakabayashi <kenichi@crssrds.jp>
2024-06-04 19:55:50 +0900

    NeoPixel処理と画像表示を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
-#include <Arduino.h>
+#include <M5Stack.h>
+// #include <M5Unified.h>
+#include <Adafruit_NeoPixel.h>
+#define PIN 21        //INが接続されているピンを指定
+#define NUMPIXELS 60  //LEDの数を指定
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);  //800kHzでNeoPixelを駆動
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  M5.begin();
+  pixels.begin();  //NeoPixelを開始
+  SPIFFS.begin();
+
+  M5.Lcd.fillScreen(BLACK);  //画面を黒で塗りつぶし
+  M5.Lcd.setTextColor(NAVY, PINK);  //文字色を白に設定
+  M5.Lcd.setTextSize(2);  //文字サイズを2に設定
+  M5.Lcd.drawJpgFile(SPIFFS, "/02.jpg", 0, 0);
+  M5.Lcd.setCursor(5, 220);  //カーソルを(0, 0)に設定
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  M5.update();
+  for (int j = 0; j < NUMPIXELS; j++) {
+    for (int i = 0; i < NUMPIXELS; i++) {
+      int pos = (i + j) % NUMPIXELS;  //LEDの位置を計算
+      pixels.setPixelColor(pos, pixels.ColorHSV((65535 / NUMPIXELS)* i , 255, 50));  //全てのLEDを消灯
+      pixels.show();  //LEDに色を反映
+    }
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
