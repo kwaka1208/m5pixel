03baadec55fe346494c1c736d7e70fc9fc841bb9
Kenichi Wakabayashi <kenichi@crssrds.jp>
2024-06-15 12:46:06 +0900

    Wi-Fi設定があれば日付表示を動かす＆Dojo仕様を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,53 @@
 #include <M5Stack.h>
-// #include <M5Unified.h>
 #include <Adafruit_NeoPixel.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include "wifi_setting.h"
+// #include <HTTPClient.h>
+
 #define PIN 21        //INが接続されているピンを指定
-#define NUMPIXELS 30  //LEDの数を指定
+#define NUMPIXELS 60  //LEDの数を指定
+
+
+void ShowTime(struct tm *timeInfo);
+// void ShowURLImage(const char* url);
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);  //800kHzでNeoPixelを駆動
+struct tm timeInfo;  //時刻を格納するオブジェクト
 
 void setup() {
   M5.begin();
-  pixels.begin();  //NeoPixelを開始
-  SPIFFS.begin();
+  M5.Lcd.fillScreen(BLACK);  // 塗りつぶし
+  M5.Lcd.setTextColor(WHITE,BLACK); //文字色、背景色
+  M5.Lcd.setTextSize(2);  //文字サイズ
+  M5.Lcd.setRotation(2);
+  M5.Lcd.setCursor(10, 160);  //カーソル位置指定
+  M5.Lcd.println("Welcome to MStack");
 
-  M5.Lcd.fillScreen(BLACK);  //画面を黒で塗りつぶし
-  M5.Lcd.setTextColor(NAVY, PINK);  //文字色を白に設定
-  M5.Lcd.setTextSize(2);  //文字サイズを2に設定
-  M5.Lcd.setCursor(5, 220);  //カーソルを(0, 0)に設定
-  M5.Lcd.drawJpgFile(SPIFFS, "/01.jpg", 0, 0);
+  pixels.begin();  //NeoPixelを開始
+
+#ifdef SSID
+  WiFi.mode(WIFI_AP_STA);
+
+  bool is_connected = false;
+  WiFi.begin(SSID, PASS);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(500);
+    M5.Lcd.print(".");
+    // M5.update();
+    // if (M5.BtnA.isPressed()) break;
+  }
+  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+    is_connected = true;
+    M5.Lcd.println("Wi-Fi Connected");
+    configTime(9 * 3600L, 0, "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");  //NTPの設定
+    getLocalTime(&timeInfo);  //tmオブジェクトのtimeInfoに現在時刻を入れ込む
+  }
+  // M5.Lcd.drawJpgFile(SD, "/320x240/s/coderdojo02.jpg", 0, 0);
+  if(is_connected) {
+    ShowTime(&timeInfo);
+  }
+#endif
 }
 
 void loop() {
@@ -26,15 +58,20 @@ void loop() {
       pixels.show();  //LEDに色を反映
       M5.update();
       if (M5.BtnA.isPressed()) {
-        M5.Lcd.drawJpgFile(SPIFFS, "/01.jpg", 0, 0);
+        M5.Lcd.drawJpgFile(SD, "/240x320/dojo/dojo01.jpg", 0, 0);
       }
       if (M5.BtnB.isPressed()) {
-        M5.Lcd.drawJpgFile(SPIFFS, "/02.jpg", 0, 0);
+        M5.Lcd.drawJpgFile(SD, "/240x320/dojo/dojo02.jpg", 0, 0);
       }
       if (M5.BtnC.isPressed()) {
-        M5.Lcd.drawJpgFile(SPIFFS, "/03.jpg", 0, 0);
+        M5.Lcd.drawJpgFile(SD, "/240x320/dojo/dojo03.jpg", 0, 0);
       }
     }
   }
 }
 
+void ShowTime(struct tm *timeInfo) {
+  String time_str = String(timeInfo->tm_year + 1900) + "/" + String(timeInfo->tm_mon + 1) + "/" + String(timeInfo->tm_mday);
+  M5.Lcd.setCursor(60, 180);  //カーソル位置指定
+  M5.Lcd.println(time_str);
+  }
